611de2597a9019c512edea2aff682fae58aadd09
Raphael <raphawestim45@gmail.com>
2024-04-03 16:38:19 -0300

    ajustes e copy de textos do site

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
+function scrollVisibleWeb(){
+  const sections = document.querySelectorAll(".item-conteudo");
+  console.log(sections);
+
+  if(sections.length){
+      const windowMetade = window.innerHeight * 0.7;
+      console.log(sections);
+
+      function animaScroll(){
+          sections.forEach((section) => {
+              const sectionTop = section.getBoundingClientRect().top;
+              const isSectionVisible = sectionTop - windowMetade < 0;
+              //console.log(sectionTop)
+              
+              if(isSectionVisible){
+                  section.classList.add('ativo');
+              }else{
+                  section.classList.remove('ativo');
+              }
+          });
+      }
+
+      animaScroll();
+
+      window.addEventListener('scroll', animaScroll)
+  }
+};
+
+// Função para verificar se uma seção está visível na janela de visualização
+scrollVisibleWeb();
+
+
 const menuMobile = document.querySelector('.menu-mobile');
 console.log(menuMobile);
 
@@ -6,3 +38,15 @@ menuMobile.addEventListener('click', () => {
   const menu = document.querySelector('.menu');
   menu.classList.toggle('active');
 });
+
+
+const whatsapp = document.querySelector('.whatsapp');
+whatsapp.addEventListener('click', () => {
+  console.log('Clicou no whatsapp');
+
+  const numeroTelefone  = '5511957938716'
+  const linkWhats = `https://wa.me//${numeroTelefone}?text=Olá%20gostaria%20de%20fazer%20um%20orçamento!`;
+
+  window.open(linkWhats, '_blank');
+});
+
